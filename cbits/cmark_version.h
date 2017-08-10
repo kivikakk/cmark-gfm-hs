e1f03932061aeb173d83f0163ffc6940c4b5cd67
Yuki Izumi <ashe@kivikakk.ee>
2017-08-10 13:57:41 +1000

    Update to cmark 0.28.0.gfm.6

--- cbits/cmark_version.h
+++ cbits/cmark_version.h
@@ -1,7 +1,8 @@
 #ifndef CMARK_VERSION_H
 #define CMARK_VERSION_H
 
-#define CMARK_VERSION ((0 << 16) | (28 << 8)  | 0)
-#define CMARK_VERSION_STRING "0.28.0"
+#define CMARK_VERSION ((0 << 24) | (28 << 16) | (0 << 8) | 5)
+#define CMARK_VERSION_STRING "0.28.0.gfm.5"
+#define CMARK_GFM_VERSION 5
 
 #endif
