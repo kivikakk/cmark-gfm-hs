7b8fbbc9a92781c38d208e38a2a9f5a6348ad16c
Asherah Connor <ashe@kivikakk.ee>
2025-08-04 14:44:58 +1000

    flake.nix: reintroduce.

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    haskell-flake.url = "github:srid/haskell-flake";
+  };
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      imports = [ inputs.haskell-flake.flakeModule ];
+
+      perSystem =
+        { self', ... }:
+        {
+          haskellProjects.default = { };
+
+          packages.default = self'.packages.cmark-gfm;
+        };
+    };
+}
